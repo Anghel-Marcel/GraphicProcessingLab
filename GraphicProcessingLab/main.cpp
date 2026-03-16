6f58da60c1e24fcad70fa3528b34187301ff2cc9
Anghel-Marcel <anghel.marcel.t8t@student.ucv.ro>
2026-03-16 16:42:01 +0200

    Add a Fixed Camera,grass and sky textures

--- GraphicProcessingLab/main.cpp
+++ GraphicProcessingLab/main.cpp
@@ -3,39 +3,96 @@
 #include <iostream>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
+#include "stb_image.h"
 
-// Ground vertices (position + texture coordinates)
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// -------------------- Ground --------------------
+
 float groundVertices[] = {
-    -0.8f, -0.8f, 0.0f, 0.0f, 0.0f,
-     0.8f, -0.8f, 0.0f, 10.0f, 0.0f,
-     0.8f,  0.8f, 0.0f, 10.0f,10.0f,
 
-     0.8f,  0.8f, 0.0f, 10.0f,10.0f,
-    -0.8f,  0.8f, 0.0f, 0.0f,10.0f,
-    -0.8f, -0.8f, 0.0f, 0.0f, 0.0f
+    -5.0f,0.0f,-5.0f,   0.0f,0.0f,
+     5.0f,0.0f,-5.0f,   5.0f,0.0f,
+     5.0f,0.0f, 5.0f,   5.0f,5.0f,
+
+     5.0f,0.0f, 5.0f,   5.0f,5.0f,
+    -5.0f,0.0f, 5.0f,   0.0f,5.0f,
+    -5.0f,0.0f,-5.0f,   0.0f,0.0f
 };
 
-// Vertex shader
+// -------------------- Terrain --------------------
+
+float terrainVertices[] = {
+
+    -2.0f,0.0f,-1.0f, 0.0f,0.0f,
+    -1.0f,0.6f, 0.0f, 0.5f,1.0f,
+     0.0f,0.0f,-1.0f, 1.0f,0.0f,
+
+     0.0f,0.0f,-1.0f, 1.0f,0.0f,
+     1.0f,0.5f, 0.0f, 0.5f,1.0f,
+     2.0f,0.0f,-1.0f, 0.0f,0.0f
+};
+
+// -------------------- Skybox walls --------------------
+
+float skyboxVertices[] = {
+
+    // Back wall
+    -10.0f,0.0f,-10.0f, 0.0f,0.0f,
+     10.0f,0.0f,-10.0f, 1.0f,0.0f,
+     10.0f,10.0f,-10.0f,1.0f,1.0f,
+
+     10.0f,10.0f,-10.0f,1.0f,1.0f,
+    -10.0f,10.0f,-10.0f,0.0f,1.0f,
+    -10.0f,0.0f,-10.0f,0.0f,0.0f,
+
+    // Left wall
+    -10.0f,0.0f,10.0f,0.0f,0.0f,
+    -10.0f,0.0f,-10.0f,1.0f,0.0f,
+    -10.0f,10.0f,-10.0f,1.0f,1.0f,
+
+    -10.0f,10.0f,-10.0f,1.0f,1.0f,
+    -10.0f,10.0f,10.0f,0.0f,1.0f,
+    -10.0f,0.0f,10.0f,0.0f,0.0f,
+
+    // Right wall
+     10.0f,0.0f,-10.0f,0.0f,0.0f,
+     10.0f,0.0f,10.0f,1.0f,0.0f,
+     10.0f,10.0f,10.0f,1.0f,1.0f,
+
+     10.0f,10.0f,10.0f,1.0f,1.0f,
+     10.0f,10.0f,-10.0f,0.0f,1.0f,
+     10.0f,0.0f,-10.0f,0.0f,0.0f
+};
+
+// -------------------- Shaders --------------------
+
 const char* vertexShaderSource = R"(
+
 #version 330 core
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec2 aTexCoord;
+layout (location = 1) in vec2 aTex;
 
 out vec2 TexCoord;
 
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
-    gl_Position = vec4(aPos, 1.0);
-    TexCoord = aTexCoord;
+    gl_Position = projection * view * vec4(aPos,1.0);
+    TexCoord = aTex;
 }
+
 )";
 
-// Fragment shader
 const char* fragmentShaderSource = R"(
-#version 330 core
 
+#version 330 core
 out vec4 FragColor;
+
 in vec2 TexCoord;
 
 uniform sampler2D texture1;
@@ -44,63 +101,87 @@ void main()
 {
     FragColor = texture(texture1, TexCoord);
 }
+
 )";
+
+// -------------------- Texture Loader --------------------
+
+unsigned int loadTexture(const char* path)
+{
+    unsigned int texture;
+
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, channels;
+    unsigned char* data = stbi_load(path, &width, &height, &channels, 0);
+
+    if (data)
+    {
+        GLenum format = channels == 4 ? GL_RGBA : GL_RGB;
+
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Texture failed: " << path << std::endl;
+    }
+
+    stbi_image_free(data);
+
+    return texture;
+}
+
+// -------------------- MAIN --------------------
 
 int main()
 {
+
     glfwInit();
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Textured Ground", NULL, NULL);
-
-    if (!window)
-    {
-        std::cout << "Failed to create window\n";
-        glfwTerminate();
-        return -1;
-    }
-
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Scene Cube", NULL, NULL);
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD\n";
-        return -1;
-    }
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-    glViewport(0, 0, 800, 600);
+    glEnable(GL_DEPTH_TEST);
 
-    // ----- Shaders -----
+    stbi_set_flip_vertically_on_load(true);
 
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
+    // -------------------- Compile Shaders --------------------
 
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
+    unsigned int vShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vShader);
+
+    unsigned int fShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fShader);
 
     unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
+    glAttachShader(shaderProgram, vShader);
+    glAttachShader(shaderProgram, fShader);
     glLinkProgram(shaderProgram);
 
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    glDeleteShader(vShader);
+    glDeleteShader(fShader);
 
-    // ----- VAO + VBO -----
+    // -------------------- VAOs --------------------
 
-    unsigned int VAO, VBO;
+    unsigned int groundVAO, groundVBO;
 
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &groundVAO);
+    glGenBuffers(1, &groundVBO);
 
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindVertexArray(groundVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, groundVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(groundVertices), groundVertices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
@@ -109,46 +190,84 @@ int main()
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    // ----- Texture -----
+    unsigned int terrainVAO, terrainVBO;
 
-    unsigned int texture;
+    glGenVertexArrays(1, &terrainVAO);
+    glGenBuffers(1, &terrainVBO);
 
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
+    glBindVertexArray(terrainVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, terrainVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(terrainVertices), terrainVertices, GL_STATIC_DRAW);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
-    int width, height, channels;
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
-    unsigned char* data = stbi_load("textures/grass.jpg", &width, &height, &channels, 0);
+    unsigned int skyVAO, skyVBO;
 
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture\n";
-    }
+    glGenVertexArrays(1, &skyVAO);
+    glGenBuffers(1, &skyVBO);
 
-    stbi_image_free(data);
+    glBindVertexArray(skyVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), skyboxVertices, GL_STATIC_DRAW);
 
-    // ----- Render loop -----
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // -------------------- Textures --------------------
+
+    unsigned int grassTex = loadTexture("textures/grass.jpg");
+    unsigned int skyTex = loadTexture("textures/sky.jpg");
+
+    // -------------------- Render Loop --------------------
 
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(0.4f, 0.7f, 1.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+
+        glClearColor(0.5f, 0.7f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
 
-        glBindTexture(GL_TEXTURE_2D, texture);
-        glBindVertexArray(VAO);
+        // -------------------- Camera (STATIC for P1) --------------------
 
+        glm::mat4 view = glm::lookAt(
+            glm::vec3(0.0f, 3.0f, 8.0f),
+            glm::vec3(0.0f, 0.0f, 0.0f),
+            glm::vec3(0.0f, 1.0f, 0.0f)
+        );
+
+        glm::mat4 projection = glm::perspective(
+            glm::radians(45.0f),
+            800.0f / 600.0f,
+            0.1f,
+            100.0f
+        );
+
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+        // -------------------- SKY --------------------
+
+        glBindTexture(GL_TEXTURE_2D, skyTex);
+        glBindVertexArray(skyVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 18);
+
+        // -------------------- GROUND --------------------
+
+        glBindTexture(GL_TEXTURE_2D, grassTex);
+        glBindVertexArray(groundVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        // -------------------- TERRAIN --------------------
+
+        glBindVertexArray(terrainVAO);
         glDrawArrays(GL_TRIANGLES, 0, 6);
 
         glfwSwapBuffers(window);
